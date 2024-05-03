594b22a129f3de0fdc2cee0f7fcd2ea9a7788bac
Thiago <thiago.sbr@hotmail.com>
2024-05-03 10:43:07 -0300

    .

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 app.use(cors());
+var results = [];
 app.get("/", function (req, res) { return res.json("Express on Vercel"); });
+app.get("/api/reset", function (req, res) {
+    results = [];
+    res.json(results);
+});
+app.get("/api/users", function (req, res) {
+    var params = req.query;
+    if (!params.q) {
+        return res.json(results);
+    }
+    var data = results.filter(function (result) {
+        return (result.name.toLowerCase().includes(params.q.toLowerCase()) ||
+            result.city.toLowerCase().includes(params.q.toLowerCase()) ||
+            result.country.toLowerCase().includes(params.q.toLowerCase()) ||
+            result.favorite_sport.toLowerCase().includes(params.q.toLowerCase()));
+    });
+    res.json(data);
+});
 app.listen(3000, function () { return console.log("Server ready on port 3000."); });
 module.exports = app;
--- index.ts
+++ index.ts
@@ -4,7 +4,38 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+let results: {
+    name: string;
+    city: string;
+    country: string;
+    favorite_sport: string;
+  }[] = [];
+
 app.get("/", (req, res) => res.json("Express on Vercel"));
+
+app.get("/api/reset", (req: any, res: any) => {
+    results = [];
+    res.json(results);
+  });
+
+  app.get("/api/users", (req: any, res: any) => {
+    const params = req.query;
+  
+    if (!params.q) {
+      return res.json(results);
+    }
+  
+    const data = results.filter(result => {
+      return (
+        result.name.toLowerCase().includes(params.q.toLowerCase()) ||
+        result.city.toLowerCase().includes(params.q.toLowerCase()) ||
+        result.country.toLowerCase().includes(params.q.toLowerCase()) ||
+        result.favorite_sport.toLowerCase().includes(params.q.toLowerCase())
+      );
+    });
+  
+    res.json(data);
+  });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
