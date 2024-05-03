5e69fd0db354d2f2b634e90f196e3ba999033aca
Thiago <thiago.sbr@hotmail.com>
2024-05-03 10:41:00 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express");
+var cors = require("cors");
 var app = express();
+app.use(cors());
 app.get("/", function (req, res) { return res.json("Express on Vercel"); });
 app.listen(3000, function () { return console.log("Server ready on port 3000."); });
 module.exports = app;
